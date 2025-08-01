e621e847787a6d73106fb3cfe6434a60842fbe76
GoldenAppler <64623388+GoldenAppler-eng@users.noreply.github.com>
2025-08-01 17:59:28 +1000

    Added vertical coordinate based attack range

--- script/component/DamagerComponent.gd
+++ script/component/DamagerComponent.gd
@@ -1,8 +1,26 @@
 class_name DamagerComponent
 extends Area2D
 
+@export var reach : float = 0.4
+
 func deal_damage() -> void:	
+	var rotation_tracker_meta_name : StringName = GlobalConstants.get_component_name(GlobalConstants.COMPONENT.ROTATIONTRACKER)
+	
 	for area in get_overlapping_areas():
-		if area is HurtboxComponent:
-			var hurtbox_component : HurtboxComponent = area as HurtboxComponent
-			hurtbox_component.get_hurt()
+		if not area is HurtboxComponent:
+			continue
+			
+		if area.owner.has_meta(rotation_tracker_meta_name) and owner.has_meta(rotation_tracker_meta_name):
+			var attacker_rotation_tracker_component : RotationTrackerComponent = owner.get_meta(rotation_tracker_meta_name)
+			var attacked_rotation_tracker_component : RotationTrackerComponent = area.owner.get_meta(rotation_tracker_meta_name)
+		
+			var attacker_vertical_coordinate : float = attacker_rotation_tracker_component.psuedo_vertical_coordinate
+			var attacked_vertical_coordinate : float = attacked_rotation_tracker_component.psuedo_vertical_coordinate
+			
+			var vertical_distance : float = abs(attacker_vertical_coordinate - attacked_vertical_coordinate)
+			
+			if vertical_distance > reach:
+				continue
+				
+		var hurtbox_component : HurtboxComponent = area as HurtboxComponent
+		hurtbox_component.get_hurt()
