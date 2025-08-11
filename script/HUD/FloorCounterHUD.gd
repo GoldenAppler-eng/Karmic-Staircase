586bcdfdf8516743a27ff5b4fdfbfb0521cdf71c
GoldenAppler <64623388+GoldenAppler-eng@users.noreply.github.com>
2025-08-11 14:25:39 +1000

    Capped floor counter

--- script/HUD/FloorCounterHUD.gd
+++ script/HUD/FloorCounterHUD.gd
@@ -3,4 +3,4 @@ extends Label
 @export var staircase_level_manager : StaircaseLevelManager
 
 func _physics_process(delta: float) -> void:
-	text = "Floor: " + str(staircase_level_manager.current_level + 1) + "/" + str(staircase_level_manager.number_of_levels)
+	text = "Floor: " + str(min(staircase_level_manager.current_level + 1, staircase_level_manager.number_of_levels)) + "/" + str(staircase_level_manager.number_of_levels)
