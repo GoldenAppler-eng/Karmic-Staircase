faf3a1c334e6c4fd52c290696b509438d69ff65a
GoldenAppler <64623388+GoldenAppler-eng@users.noreply.github.com>
2025-08-13 13:33:38 +1000

    Added resolution selector and made theme more consistent

---
+++ script/UI/ResolutionSelector.gd
@@ -0,0 +1,31 @@
+extends OptionButton
+
+const RESOLUTIONS : Dictionary = {
+	"960x540" : Vector2i(960, 540),
+	"1280x720" : Vector2i(1280, 720),
+	"1600x900" : Vector2i(1600, 900),
+	"1920x1080" : Vector2i(1920, 1080),
+	"2560x1440" : Vector2i(2560, 1440),
+	"3840x2160" : Vector2i(3840, 2160)
+}
+
+func _ready() -> void:
+	var index : int = 0
+	for resolution in RESOLUTIONS.keys():
+		add_item(resolution)
+		set_item_metadata(index, RESOLUTIONS[resolution])
+		
+		if RESOLUTIONS[resolution] == DisplayServer.window_get_size():
+			select(index)
+		
+		index += 1
+
+	var pm : PopupMenu = get_popup()
+	
+	for i in pm.item_count:
+		pm.set_item_as_radio_checkable(i, false)
+
+func _on_item_selected(index: int) -> void:
+	var resoultion : Vector2i = get_item_metadata(index)
+	
+	DisplayServer.window_set_size(resoultion)
